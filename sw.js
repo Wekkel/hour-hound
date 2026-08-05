d59d14abb2f662aaf070fadfafb07a51b1481166
Wekkel <wekkel_e@hotmail.com>
2026-08-05 19:08:21 +0200

    Bump version to 0.1.6
    Updated version number to 0.1.6 with bug fixes and logging.

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    automatisch uit deze regel.
    ============================================================ */
 
-const VERSION = "0.1.5"; // adjustments based on first full day feedback
+const VERSION = "0.1.6"; // bug fixes and added logging
 
 const CACHE = "hourhound-" + VERSION;
 const ASSETS = [
